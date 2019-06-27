c2aaec9f9a509a89007f91061697ac65ba0b5647
Reza Lotfi <reza.lotfi14@gmail.com>
2019-06-26 18:20:33 -0700

    Add functionality for all weekday averages

--- github.js
+++ github.js
@@ -20,6 +20,11 @@ rl.prompt();
 rl.on('line', input => {
   if (!repo.owner) {
     
+    //check for valid input
+    if (!input) {
+      throw new Error('please input a valid repo owner username')
+    }
+
     //first input for repo owner
     repo.owner = input;
     
@@ -31,17 +36,22 @@ rl.on('line', input => {
 
   } else if (!repo.name) {
     
+    //check for valid input
+    if (!input) {
+      throw new Error('please enter a valid repo name')
+    }
+
     //second input for repo name
     repo.name = input;
 
     //change prompt to ask for number of weeks of repo history
-    rl.setPrompt('enter the number of weeks of history you want to see (press enter to default to 1 year) (number must be between 1 and 52): ');
+    rl.setPrompt('enter the number of weeks of history you want to see (number must be between 1 and 52) (press enter to default to 1 year): ');
 
     //prompt user again
     rl.prompt();
 
   } else if (!repo.weeks) {
-
+    
     //check if the default value was chosen. if it wasn't, check if it is a valid input. if it is, set that number of weeks.
     if (!input) {
       repo.weeks = 52;
@@ -51,32 +61,60 @@ rl.on('line', input => {
       repo.weeks = input;
     }
 
+    //change the prompt to ask for sorted values in ascending or descending order
+    rl.setPrompt('please specify if you\'d like to see the commit counts for each weekday in ascending or descending order (enter \'asc\' or \'desc\') (press enter to default to descending): ');
+
+    //prompt user again
+    rl.prompt();
+
+  } else if (!repo.sort) {
+
+    //check if the input is valid, then set the sort property accordingly
+    if (input !== 'asc' && input !== 'desc' && input) {
+      throw new Error('please enter only \'asc\' or \'desc\' (no quotation marks) or just press enter');
+    } else if (!input || input === 'desc') {
+      repo.sort = 'desc';
+    } else {
+      repo.sort = 'asc';
+    }
+
     //now make a GET request to the GitHub API
     axios.get(`https://api.github.com/repos/${repo.owner}/${repo.name}/stats/commit_activity`)
       .then(response => {
 
-        //initialize commit counter array
-        let dayCommits = [0, 0, 0, 0, 0, 0, 0];
-  
+        //initialize commit counter object
+        let dayCommits = {};
+
         //count the total number of commits for each day of the week over the last year
         for (let i = 0; i < days.length; i++) {
+          if (!dayCommits[days[i]]) {
+            dayCommits[days[i]] = 0;
+          }
           for (let j = 0; j < repo.weeks; j++) {
-            dayCommits[i] += response.data[j].days[i];
+            dayCommits[days[i]] += response.data[j].days[i];
           }
         }
   
         return dayCommits;
       })
       .then(dayCommits => {
-        //find the highest number of commits for a day of the week (this gets used later)
-        const mostCommitsOnADayOfWeek = Math.max(...dayCommits);
         
-        //find the day of the week with the most commits
-        const mostCommittedDay = days[dayCommits.indexOf(mostCommitsOnADayOfWeek)];
-  
-        //output the findings to the user
-        console.log(`\n${mostCommittedDay} has had the most commits, ${mostCommitsOnADayOfWeek}, over the last ${repo.weeks} weeks.`);
-        console.log(`On average, about ${(mostCommitsOnADayOfWeek/`${repo.weeks}`).toFixed(2)} commits were made on every ${mostCommittedDay} over the last ${repo.weeks} weeks.`);
+        //initialize an array of the weekdays sorted by their commit number
+        let sortedDays;
+
+        //populate that array with weekdays in either ascending or descending order
+        if (repo.sort === 'asc') {
+          sortedDays = Object.keys(dayCommits).sort((a, b) => {return dayCommits[a] - dayCommits[b]});
+        } else {
+          sortedDays = Object.keys(dayCommits).sort((a, b) => {return dayCommits[b] - dayCommits[a]});
+        }
+
+        console.log(`\nThe following are the average number of commits made per weekday over the last ${repo.weeks} weeks in ${repo.sort}ending order for ${repo.owner}\'s repo named \'${repo.name}\':\n`);
+
+        //loop through the sorted array of weekdays and print each weekday's corresponding commit count average
+        for (let i = 0; i < sortedDays.length; i++) {
+          console.log(`${sortedDays[i]}, ${(dayCommits[sortedDays[i]]/`${repo.weeks}`).toFixed(2)} commits`)
+        }
   
         //return to the command line
         process.exit();
